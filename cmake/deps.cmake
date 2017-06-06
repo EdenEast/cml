f13f7b9d2ead1e5ab86d4d2161e7cb19093d5df1
tim <timothee.feuillet@gmail.com>
2017-06-05 21:40:40 -0400

    [cml]: initial commit  - cmake structure done  - gitignore file  - sample folder

---
+++ cmake/deps.cmake
@@ -0,0 +1,10 @@
+
+# set(CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake")
+
+
+# deps libs (for exec)
+set(PROJ_DEPS_LIBS )
+
+# include dirs
+set(PROJ_INCLUDE_DIRS )
+
---
+++ cmake/flags.cmake
@@ -0,0 +1,40 @@
+##
+## CMAKE file for neam projects
+##
+
+
+# general flags
+if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+  set(PROJ_FLAGS "${PROJ_FLAGS} -O0 -g3")
+else(CMAKE_BUILD_TYPE STREQUAL "Release")
+  set(PROJ_FLAGS "${PROJ_FLAGS} -DNDEBUG ")
+else(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+  set(PROJ_FLAGS "${PROJ_FLAGS} -DNDEBUG ")
+endif()
+
+# some gcc/clang flags
+if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+  set(PROJ_FLAGS "${PROJ_FLAGS} -std=c++1z -Wall -Wextra -Wno-unused-function")
+
+  if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(PROJ_FLAGS "${PROJ_FLAGS} -Og -fno-inline")
+  else(CMAKE_BUILD_TYPE STREQUAL "Release")
+    set(PROJ_FLAGS "${PROJ_FLAGS} -O3 -finline-limit=100")
+  else(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+    set(PROJ_FLAGS "${PROJ_FLAGS} -Os ")
+  endif()
+
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+  set(PROJ_FLAGS "${PROJ_FLAGS} -std=c++1z -Wall -Wextra -Wno-unused-function")
+
+  if (CMAKE_BUILD_TYPE STREQUAL "Debug")
+    set(PROJ_FLAGS "${PROJ_FLAGS}")
+  else(CMAKE_BUILD_TYPE STREQUAL "Release")
+    set(PROJ_FLAGS "${PROJ_FLAGS} -O3")
+  else(CMAKE_BUILD_TYPE STREQUAL "MinSizeRel")
+    set(PROJ_FLAGS "${PROJ_FLAGS} -Oz")
+  endif()
+endif()
+
+
+
