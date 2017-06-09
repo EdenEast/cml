ecf16eaac67bd79f89c52b90deaf4f3e6c9b6015
tim <timothee.feuillet@gmail.com>
2017-06-08 23:46:55 -0400

    [matrix/transpose]: Add transpose

---
+++ cml/functions/transpose.hpp
@@ -0,0 +1,46 @@
+//
+// Copyright (c) 2017 James Simpson, Timothée Feuillet
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+#pragma once
+
+#include "../matrix.hpp"
+
+namespace cml
+{
+    namespace implementation
+    {
+        template<typename ValueType, size_t DimX, size_t DimY, size_t... Idxs>
+        constexpr matrix<DimY, DimX, ValueType> transpose_impl(std::index_sequence<Idxs...>, const implementation::matrix<DimX, DimY, ValueType> &m)
+        {
+            return matrix<DimY, DimX, ValueType>
+            {
+                m.components[(Idxs / DimX) + (Idxs % DimX) * DimY]...
+            };
+        }
+    } // namespace implementation
+
+    template<typename ValueType, size_t DimX, size_t DimY>
+    constexpr implementation::matrix<DimY, DimX, ValueType> transpose(const implementation::matrix<DimX, DimY, ValueType>& m)
+    {
+        return implementation::transpose_impl(std::make_index_sequence<DimX * DimY>{}, m);
+    }
+} // namespace cml
