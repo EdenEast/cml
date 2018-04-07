88b9d8b061376eb48f0c06d427f3977f6131ea3c
James Simpson <james.simpson9339@gmail.com>
2018-04-07 11:01:13 -0400

    implement scaffold

--- cmake/filter_group.cmake
+++
@@ -1,9 +0,0 @@
-
-macro(add_filter_group source_list root_dir)
-    foreach(source ${source_list})
-        get_filename_component(parent_dir ${source} DIRECTORY)
-        file(RELATIVE_PATH group "${root_dir}" "${parent_dir}")
-        string(REPLACE "/" "\\" group "${group}")
-        source_group("${group}" FILES "${source}")
-    endforeach()
-endmacro()
